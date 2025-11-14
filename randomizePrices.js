689c449f21480a92863f4c3add1bf66499bd5ea4
Leonard <leonardshiks15@gmail.com>
2025-11-14 19:45:45 +0200

    Update display prices to '5 rand' or '10 rands'

--- randomizePrices.js
+++ randomizePrices.js
@@ -4,7 +4,7 @@ const products = JSON.parse(fs.readFileSync('products.json', 'utf8'));
 
 products.forEach(p => {
   p.price = Math.random() < 0.5 ? 5 : 10;
-  p.display_price = `R ${p.price}.00`;
+  p.display_price = p.price === 5 ? '5 rand' : '10 rands';
 });
 
 fs.writeFileSync('products.json', JSON.stringify(products, null, 2));
