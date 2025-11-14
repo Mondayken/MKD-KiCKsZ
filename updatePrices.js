acab1c400980cb4b0305d2ea34d48eff0d5006a9
Leonard <leonardshiks15@gmail.com>
2025-11-14 19:16:51 +0200

    Update product prices randomly to 5 or 10 ZAR

--- updatePrices.js
+++ updatePrices.js
@@ -3,9 +3,11 @@ const fs = require('fs');
 const products = JSON.parse(fs.readFileSync('products.json', 'utf8'));
 
 products.forEach(product => {
-  product.price = 1000;
-  product.display_price = 'R 1000.00';
+  // Randomly assign price to 5 or 10 ZAR
+  const randomPrice = Math.random() < 0.5 ? 5 : 10;
+  product.price = randomPrice;
+  product.display_price = `R ${randomPrice}.00`;
 });
 
 fs.writeFileSync('products.json', JSON.stringify(products, null, 2));
-console.log('Prices updated to 1000 ZAR');
+console.log('Prices randomly updated to 5 or 10 ZAR');
